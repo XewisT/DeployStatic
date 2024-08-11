dadd664ed79bfcda88ba33b5b43da72801ca8df1
XewisT <gether255@gmail.com>
2024-08-11 22:33:52 +0300

    fix

--- infst/route53.tf
+++ infst/route53.tf
@@ -16,15 +16,6 @@ resource "aws_route53_record" "root" {
   }
 }
 
-# # Create subdomain for prod environment
-# resource "aws_route53_record" "prod" {
-#   zone_id = data.aws_route53_zone.main.zone_id
-#   name    = "prod.${var.root_domain_name}"
-#   type    = "CNAME"
-#   ttl     = 300
-#   records = [aws_cloudfront_distribution.prod_distribution.domain_name]
-# }
-
 # Create subdomain for dev environment
 resource "aws_route53_record" "dev" {
   zone_id = data.aws_route53_zone.main.zone_id
@@ -37,7 +28,7 @@ resource "aws_route53_record" "dev" {
 # Create DNS records for RDS
 resource "aws_route53_record" "rds_subdomain" {
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = "rds.prod.dev-vysh.com"
+  name    = "rds.dev-vysh.com"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_lb.app_lb.dns_name]
@@ -46,7 +37,7 @@ resource "aws_route53_record" "rds_subdomain" {
 # Create DNS records for Redis
 resource "aws_route53_record" "redis_subdomain" {
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = "redis.prod.dev-vysh.com"
+  name    = "redis.dev-vysh.com"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_lb.app_lb.dns_name]
