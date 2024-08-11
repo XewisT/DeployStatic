f3efed14f60c837a6e8c8a06a56a0918fd743497
XewisT <gether255@gmail.com>
2024-08-11 14:45:28 +0300

    fix

--- infst/load_balancer.tf
+++ infst/load_balancer.tf
@@ -11,20 +11,23 @@ resource "aws_lb" "app_lb" {
 
 resource "aws_lb_listener" "https_rds" {
   load_balancer_arn = aws_lb.app_lb.arn
-  port              = 8080
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate.app_cert.arn
-
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.rds.arn
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
 resource "aws_lb_listener" "https_redis" {
   load_balancer_arn = aws_lb.app_lb.arn
-  port              = 8081
+  port              = 443
   protocol          = "HTTPS"
   certificate_arn   = aws_acm_certificate.app_cert.arn
 
@@ -93,8 +96,8 @@ resource "aws_lb_listener_rule" "rds_rule" {
   }
 
   condition {
-    path_pattern {
-      values = ["prod.dev-vysh.com"]
+    host_header {
+      values = ["rdsprod.dev-vysh.com"]
     }
   }
 }
@@ -109,8 +112,8 @@ resource "aws_lb_listener_rule" "redis_rule" {
   }
 
   condition {
-    path_pattern {
-      values = ["prod.dev-vysh.com"]
+    host_header {
+      values = ["redisprod.dev-vysh.com"]
     }
   }
 }
--- infst/security_groups.tf
+++ infst/security_groups.tf
@@ -4,15 +4,15 @@ resource "aws_security_group" "alb_sg" {
   vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 8081
-    to_port     = 8081
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
