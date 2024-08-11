3a85a7a9c333a13525928d8cffb9230c70e21fe7
XewisT <gether255@gmail.com>
2024-08-11 21:17:53 +0300

    fix

--- infst/route53.tf
+++ infst/route53.tf
@@ -3,6 +3,19 @@ data "aws_route53_zone" "main" {
   name = var.root_domain_name # Replace with your domain name
 }
 
+# Create A/ALIAS record for root domain
+resource "aws_route53_record" "root" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.root_domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.prod_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.prod_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 # # Create subdomain for prod environment
 # resource "aws_route53_record" "prod" {
 #   zone_id = data.aws_route53_zone.main.zone_id
@@ -24,7 +37,7 @@ resource "aws_route53_record" "dev" {
 # Create DNS records for RDS
 resource "aws_route53_record" "rds_subdomain" {
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = "rds.dev-vysh.com"
+  name    = "rds.prod.dev-vysh.com"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_lb.app_lb.dns_name]
@@ -33,7 +46,7 @@ resource "aws_route53_record" "rds_subdomain" {
 # Create DNS records for Redis
 resource "aws_route53_record" "redis_subdomain" {
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = "redis.dev-vysh.com"
+  name    = "redis.prod.dev-vysh.com"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_lb.app_lb.dns_name]
