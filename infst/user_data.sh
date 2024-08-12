855893284bfdf1717d25dee30e89b9543dfd95a6
XewisT <gether255@gmail.com>
2024-08-12 12:50:48 +0300

    fix

--- infst/user_data.sh
+++ infst/user_data.sh
@@ -6,30 +6,30 @@ apt install docker.io -y
 service docker start
 usermod -a -G docker ubuntu
 snap install yq
-echo 'services:
+sudo -u ubuntu bash -c 'echo "services:
   backend_rds:
     image: ./backend_rds
     environment:
-      CORS_ALLOWED_ORIGINS: "https://dev-vysh.com"
+      CORS_ALLOWED_ORIGINS: \"https://dev-vysh.com\"
       DB_NAME: mydatabase
       DB_PASSWORD: mypassword
       DB_USER: mydatabaseuser
       DB_HOST: db
       DB_PORT: 5432
     ports:
-      - "3000:8000"
+      - \"3000:8000\"
     depends_on:
       - db
 
   backend_redis:
     image: ./backend_redis
     environment:
-      CORS_ALLOWED_ORIGINS: "https://dev-vysh.com"
+      CORS_ALLOWED_ORIGINS: \"https://dev-vysh.com\"
       REDIS_HOST: redis
       REDIS_PORT: 6379
       REDIS_DB: 0
     ports:
-      - "3001:8000"
+      - \"3001:8000\"
     depends_on:
       - redis
 
@@ -40,18 +40,18 @@ echo 'services:
       POSTGRES_PASSWORD: mypassword
       POSTGRES_DB: mydatabase
     ports:
-      - "5432:5432"
+      - \"5432:5432\"
     volumes:
       - db_data:/var/lib/postgresql/data
 
   redis:
     image: redis:latest
     ports:
-      - "6379:6379"
+      - \"6379:6379\"
     volumes:
       - redis_data:/data
 
 volumes:
   db_data:
   redis_data:
-' | sudo tee /home/ubuntu/docker-compose.yml > /dev/null
+" > /home/ubuntu/docker-compose.yml'
